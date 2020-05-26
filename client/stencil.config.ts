b7aca1d478ae175cdc0a625c47169d27816870fa
kflange <zknsftk1msrk@gmail.com>
2020-05-26 18:20:59 +0900

    Added: index.html for dist. Changed: stencil.config.ts for dist.

--- client/stencil.config.ts
+++ client/stencil.config.ts
@@ -6,8 +6,11 @@ export const config: Config = {
   globalScript: 'src/global/app.ts',
   globalStyle: 'src/global/app.css',
   taskQueue: 'async',
+  namespace: 'CheckSheet',
   outputTargets: [{
     type: 'www',
     serviceWorker: null
+  }, {
+    type: 'dist'
   }],
 };
